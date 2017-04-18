c06bf27fd9a3dd88be9669a3b10dd76507de77b2
Sam Clinckspoor <sam.clinckspoor@gmail.com>
2017-04-18 16:24:32 +0200

    output the website bucket arn (#7)
    * output the website bucket arn
    * update changelog

--- site-main/outputs.tf
+++ site-main/outputs.tf
@@ -9,3 +9,7 @@ output "website_cdn_zone_id" {
 output "website_bucket_id" {
   value = "${aws_s3_bucket.website_bucket.id}"
 }
+
+output "website_bucket_arn" {
+  value = "${aws_s3_bucket.website_bucket.arn}"
+}
