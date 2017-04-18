53e72ecee94303a6c7aacd98d51ec776454d7a2e
Sam Clinckspoor <sam.clinckspoor@gmail.com>
2017-04-18 15:03:22 +0200

    Out put the bucket id on site-main
    makes it possible for the bucket ID to be used outside of the
    module

--- site-main/outputs.tf
+++ site-main/outputs.tf
@@ -5,3 +5,7 @@ output "website_cdn_hostname" {
 output "website_cdn_zone_id" {
   value = "${aws_cloudfront_distribution.website_cdn.hosted_zone_id}"
 }
+
+output "website_bucket_id" {
+  value = "${aws_s3_bucket.website_bucket.id}"
+}
