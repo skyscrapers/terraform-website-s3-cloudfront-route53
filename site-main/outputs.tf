edd90ccba17010ad6b96e21f60e0c732b57f6554
Jordan Prince <jordanmprince@gmail.com>
2017-09-07 11:27:38 -0500

    Add cdn id to the output

--- site-main/outputs.tf
+++ site-main/outputs.tf
@@ -2,6 +2,11 @@ output "website_cdn_hostname" {
   value = "${aws_cloudfront_distribution.website_cdn.domain_name}"
 }
 
+output "website_cdn_id" {
+  value = "${aws_cloudfront_distribution.website_cdn.id}"
+}
+
+
 output "website_cdn_zone_id" {
   value = "${aws_cloudfront_distribution.website_cdn.hosted_zone_id}"
 }
