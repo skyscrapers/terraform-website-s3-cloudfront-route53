40078fe30da3b64f9670e2076533281222bb1fc8
Jordan Prince <jordanmprince@gmail.com>
2017-09-07 16:44:37 -0500

    Update outputs.tf

--- site-main/outputs.tf
+++ site-main/outputs.tf
@@ -6,6 +6,10 @@ output "website_cdn_id" {
   value = "${aws_cloudfront_distribution.website_cdn.id}"
 }
 
+output "website_cdn_arn" {
+  value = "${aws_cloudfront_distribution.website_cdn.arn}"
+}
+
 
 output "website_cdn_zone_id" {
   value = "${aws_cloudfront_distribution.website_cdn.hosted_zone_id}"
