9e87d5e9c745e81ad39377c16d3a422cef28c1c4
Jonathan Prouse <jonathan@gameanalytics.com>
2019-08-15 13:50:19 +0100

    adding dynamic blocks to allow for multiple origins and cloudfront behaviors

--- site-main/variables.tf
+++ site-main/variables.tf
@@ -64,3 +64,30 @@ variable "price_class" {
   default     = "PriceClass_200"
 }
 
+variable "origins" {
+  description = "Additional origins, supplementary to the default origin created from the S3 bucket"
+  default = []
+  type = list(object({
+    origin_id                        = string
+    domain_name                      = string
+    origin_path                      = string
+    origin_protocol_policy           = string
+    duplicate_content_penalty_secret = string
+  }))
+}
+
+variable "ordered_cache_behaviors" {
+  description = "Additional routing behaviors"
+  default     = []
+  type = list(object({
+    min_ttl          = string
+    default_ttl      = string
+    max_ttl          = string
+    path_pattern     = string
+    target_origin_id = string
+    cookies_forward  = string
+    event_type       = string
+    lambda_arn       = string
+    include_body     = bool
+  }))
+}
