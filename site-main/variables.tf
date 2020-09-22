72343dccf45bac338f2516e64e1f5d8d9ae49a60
Mark Kalinovits <kalee.mark@gmail.com>
2020-09-22 13:10:28 +0200

    Enables force-destroy of S3 origin bucket

--- site-main/variables.tf
+++ site-main/variables.tf
@@ -77,3 +77,9 @@ variable "minimum_client_tls_protocol_version" {
   description = "CloudFront viewer certificate minimum protocol version"
   default     = "TLSv1"
 }
+
+variable "force_destroy" {
+  type = bool
+  description = "A boolean that indicates all objects (including any locked objects) should be deleted from the bucket so that the bucket can be destroyed without error. These objects are not recoverable."
+  default = false
+}
